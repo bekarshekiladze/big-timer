e373f0b7fe489cdd964e6d59bb5086898bdde3ad
Beka <bekar.shekiladze@leverx.com>
2026-01-28 00:57:55 +0400

    wip: add more animations

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -6,6 +6,7 @@ import { useTimerStore } from "@/store/timerStore";
 import { useInitializerTimer } from "@/hooks/useInitiazerTimer";
 import useTimerTicking from "@/hooks/useTimerTicking";
 import * as motion from "motion/react-client";
+import { AnimatePresence } from "motion/react";
 
 export default function Wrapper() {
   const { isEditing, isTimerLoading } = useTimerStore();
@@ -19,6 +20,7 @@ export default function Wrapper() {
 
   return (
     <motion.div
+      key="wrapper"
       initial={{ opacity: 0, scale: 0 }}
       animate={{ opacity: 1, scale: 1 }}
       className="wrapper"
@@ -27,7 +29,29 @@ export default function Wrapper() {
         scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
       }}
     >
-      {isEditing ? <ReadingForm /> : <CountdownReading />}
+      <AnimatePresence mode="wait">
+        {isEditing ? (
+          <motion.div
+            key="readingForm"
+            initial={{ opacity: 0, scale: 0.95 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.95 }}
+            transition={{ duration: 0.15 }}
+          >
+            <ReadingForm />
+          </motion.div>
+        ) : (
+          <motion.div
+            key="countdownReading"
+            initial={{ opacity: 0, scale: 0.95 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.95 }}
+            transition={{ duration: 0.15 }}
+          >
+            <CountdownReading />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </motion.div>
   );
 }
