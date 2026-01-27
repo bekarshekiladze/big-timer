091f1fb170c720e9bf8d15b590a63d1298403a10
Beka <bekar.shekiladze@leverx.com>
2026-01-27 23:40:23 +0400

    feat: implement an motion animation

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -5,16 +5,29 @@ import { ReadingForm } from "../ReadingForm";
 import { useTimerStore } from "@/store/timerStore";
 import { useInitializerTimer } from "@/hooks/useInitiazerTimer";
 import useTimerTicking from "@/hooks/useTimerTicking";
+import * as motion from "motion/react-client";
 
 export default function Wrapper() {
-  const isEditing = useTimerStore((state) => state.isEditing);
+  const { isEditing, isTimerLoading } = useTimerStore();
 
   useInitializerTimer();
   useTimerTicking();
 
+  if (isTimerLoading) {
+    return <div className="text-xl">Loading ...</div>;
+  }
+
   return (
-    <div className="wrapper">
+    <motion.div
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      className="wrapper"
+      transition={{
+        duration: 0.4,
+        scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
+      }}
+    >
       {isEditing ? <ReadingForm /> : <CountdownReading />}
-    </div>
+    </motion.div>
   );
 }
