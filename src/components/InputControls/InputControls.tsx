85d8189ef0b9408274043ddc6d711beee5e12069
Beka <bekar.shekiladze@leverx.com>
2026-01-29 12:01:53 +0400

    fix: recreate all controll buttons

--- src/components/InputControls/InputControls.tsx
+++ src/components/InputControls/InputControls.tsx
@@ -10,13 +10,19 @@ import { FaMinus } from "react-icons/fa";
 
 export default function OnTheFlyControls() {
   const duration = useTimerStore((s) => s.duration);
+  const increment = useTimerStore((s) => s.increment);
+  const decrement = useTimerStore((s) => s.decrement);
+  const isRunning = useTimerStore((s) => s.isRunning);
+  const remainingTime = useTimerStore((s) => s.remainingTime);
   const applyTimes = useTimerStore((s) => s.applyTimes);
+
+  const base = isRunning ? remainingTime : duration;
 
   return (
     <div className="text-bigtimer-black buttons-container">
       <button
         onClick={() => {
-          applyTimes(getNextIncrementMs(duration));
+          applyTimes(getNextIncrementMs(base));
         }}
         className="place-content-center grid round button primary-button"
       >
@@ -24,7 +30,7 @@ export default function OnTheFlyControls() {
       </button>
       <button
         onClick={() => {
-          applyTimes(getNextDecrementMs(duration));
+          applyTimes(getNextDecrementMs(base));
         }}
         className="place-content-center grid round button primary-button"
       >
--- src/utils/inputControls/buttonInputControl.ts
+++ src/utils/inputControls/buttonInputControl.ts
@@ -9,55 +9,47 @@ export function clampMs(ms: number, min = MIN_MS, max = MAX_MS) {
   return Math.min(max, Math.max(min, ms));
 }
 
-export function getIncrementStepMs(durationMs: number): number {
-  const t = Math.max(0, durationMs);
+// two-way
+export function getIncrementStepMs(t: number) {
+  t = Math.max(0, t);
 
-  let stepMs: number;
+  if (t < 5 * SECOND) return 1 * SECOND;
+  if (t < 1 * MINUTE) return 5 * SECOND;
+  if (t < 20 * MINUTE) return 1 * MINUTE;
+  if (t < 1 * HOUR) return 5 * MINUTE;
 
-  if (t <= 5 * SECOND) stepMs = 1 * SECOND;
-  else if (t <= 1 * MINUTE) stepMs = 5 * SECOND;
-  else if (t <= 5 * MINUTE) stepMs = 1 * MINUTE;
-  else if (t <= 20 * MINUTE) stepMs = 1 * MINUTE;
-  else if (t <= 1 * HOUR) stepMs = 5 * MINUTE;
-  else stepMs = 15 * MINUTE;
+  return 15 * MINUTE;
+}
 
-  return stepMs;
+export function getDecrementStepMs(t: number) {
+  t = Math.max(0, t);
+
+  if (t <= 5 * SECOND) return 1 * SECOND;
+  if (t <= 1 * MINUTE) return 5 * SECOND;
+  if (t <= 20 * MINUTE) return 1 * MINUTE;
+  if (t <= 1 * HOUR) return 5 * MINUTE;
+
+  return 15 * MINUTE;
 }
 
 export function snapDown(currentMs: number, stepMs: number) {
   const t = Math.max(0, currentMs);
   const step = Math.max(1, stepMs);
-
-  const adJusted = t % step === 0 ? Math.max(0, t - 1) : t;
-
-  return Math.floor(adJusted / step) * step;
-}
-
-export function getDecrementStepMs(durationMs: number): number {
-  const t = Math.max(0, durationMs);
-
-  let stepMs: number;
-
-  if (t >= 1 * HOUR) stepMs = 15 * MINUTE;
-  else if (t >= 1 * MINUTE) stepMs = 1 * MINUTE;
-  else stepMs = 1 * SECOND;
-
-  return stepMs;
+  const adjusted = t % step === 0 ? Math.max(0, t - 1) : t; // force decrease at boundaries
+  return Math.floor(adjusted / step) * step;
 }
 
 export function snapUp(currentMs: number, stepMs: number) {
   const t = Math.max(0, currentMs);
   const step = Math.max(1, stepMs);
-
-  const adjusted = t % step === 0 ? t + 1 : t;
-
+  const adjusted = t % step === 0 ? t + 1 : t; // force progress at boundaries
   return Math.ceil(adjusted / step) * step;
 }
 
 // for increase decrease buttons only
-export function getNextIncrementMs(currentDurationMs: number) {
-  const step = getIncrementStepMs(currentDurationMs);
-  return clampMs(snapUp(currentDurationMs, step));
+export function getNextIncrementMs(currentMs: number) {
+  const step = getIncrementStepMs(currentMs);
+  return clampMs(snapUp(currentMs, step));
 }
 
 export function getNextDecrementMs(currentMs: number) {
