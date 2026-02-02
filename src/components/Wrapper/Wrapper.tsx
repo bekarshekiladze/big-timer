702bc8818315d3b66c0c7ce7331621ce8941b442
Beka <bekar.shekiladze@leverx.com>
2026-02-03 01:01:33 +0400

    feat: add loader bifore data init

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -3,13 +3,21 @@
 import TimerDisplay from "../TimerDisplay/TimerDisplay";
 import { useTimerMode } from "@/hooks/useTimerMode";
 import TimerForm from "../TimerForm/TimerForm";
+import { useTimerStore } from "@/store/timerStore";
 
 export default function Wrapper() {
   const { isEditing, enterEdit } = useTimerMode();
+  const isInitiated = useTimerStore((s) => s.isInitiated);
 
   return (
     <div onClick={!isEditing ? enterEdit : undefined}>
-      {isEditing ? <TimerForm /> : <TimerDisplay />}
+      {!isInitiated ? (
+        <div className="loader" />
+      ) : isEditing ? (
+        <TimerForm />
+      ) : (
+        <TimerDisplay />
+      )}
     </div>
   );
 }
